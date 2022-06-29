7087f8187981a475af5f8766ea5f70cc67e68bc2
Pedro Gaspar <101209561+pgasparr96@users.noreply.github.com>
2022-06-29 17:25:03 -0300

    Update CalculadoraIMC.java

--- src/main/java/teste/aula_junit/CalculadoraIMC.java
+++ src/main/java/teste/aula_junit/CalculadoraIMC.java
@@ -1,114 +1,128 @@
 package teste.aula_junit;
 
-public class CalculadoraIMC
-{
-    private double imc;
-    
-    public String calcularImc(final double peso, final double altura, final int idade, final String sexo) {
-        String retorno = "";
-        this.imc = peso / (altura * altura);
-        if (idade < 20) {
-            retorno = "";
-            if (sexo.equals("m")) {
-                if (this.imc < 5.0) {
-                    retorno = "Baixo peso";
-                }
-                else if (this.imc > 5.1 && this.imc < 85.0) {
-                    retorno = "Peso normal";
-                }
-                else if (this.imc > 85.1 && this.imc < 95.0) {
-                    retorno = "Sobrepeso";
-                }
-                else if (this.imc > 95.0) {
-                    retorno = "Obesidade";
-                }
-            }
-            if (sexo.equals("f")) {
-                if (this.imc < 5.0) {
-                    retorno = "Baixo peso";
-                }
-                else if (this.imc > 5.1 && this.imc < 85.0) {
-                    retorno = "Peso normal";
-                }
-                else if (this.imc > 85.1 && this.imc < 95.0) {
-                    retorno = "Sobrepeso";
-                }
-                else if (this.imc > 95.0) {
-                    retorno = "Obesidade";
-                }
-            }
-        }
-        else if (idade >= 20 && idade <= 65) {
-            if (this.imc < 16.0) {
-                retorno = "Baixo peso muito grave";
-            }
-            if (this.imc >= 16.0 && this.imc < 17) {
-                retorno = "Baixo peso grave";
-            }
-            if (this.imc >= 17.0 && this.imc <= 18.49) {
-                retorno = "Peso baixo";
-            }
-            else if (this.imc >= 18.50 && this.imc <= 24.99) {
-                retorno = "Peso normal";
-            }
-            else if (this.imc >= 25.0 && this.imc <= 29.99) {
-                retorno = "Sobrepeso";
-            }
-            else if (this.imc >= 30.0 && this.imc <= 34.99) {
-                retorno = "Obesidade grau 1";
-            }
-            else if (this.imc < 34.99 && this.imc >= 39.99) {
-                retorno = "Obesidade grau 2";
-            }
-            else if (this.imc > 39.99) {
-                retorno = "Obesidade grau 3";
-            }
-        }
-        else if (idade > 65) {
-            if (sexo.equals("f")) {
-                if (this.imc <= 21.9) {
-                    retorno = "Baixo peso Grave";
-                }
-                else if (this.imc > 21.9 && this.imc < 27.1) {
-                    retorno = "Peso normal";
-                }
-                else if (this.imc >= 27.1 && this.imc < 32.1) {
-                    retorno = "Sobrepeso";
-                }
-                else if (this.imc >= 32.1 && this.imc < 37.1) {
-                    retorno = "Obesidade grau 1";
-                }
-                else if (this.imc >= 37.1 && this.imc <= 41.9) {
-                    retorno = "Obesidade grau 2";
-                }
-                else if (this.imc > 41.9) {
-                    retorno = "Obesidade grau 3";
-                }
-            }
-            if (sexo.equals("m")) {
-                if (this.imc < 21.9) {
-                    retorno = "Baixo peso Grave";
-                }
-                else if (this.imc > 22.0 && this.imc < 27.0) {
-                    retorno = "Peso normal";
-                }
-                else if (this.imc > 27.1 && this.imc < 30.0) {
-                    retorno = "Sobrepeso";
-                }
-                else if (this.imc > 30.1 && this.imc < 35.0) {
-                    retorno = "Obesidade grau I";
-                }
-                else if (this.imc > 35.1 && this.imc < 39.9) {
-                    retorno = "Obesidade grau II";
-                }
-                else if (this.imc > 39.9) {
-                    retorno = "Obesidade grau III";
-                }
-            }
-        }
-        else {
-            retorno = "Valor Invalido";
-        }
-        return retorno;
-    }
+public class CalculadoraIMC {
+	
+	private double imc;
+	
+	public String calcularImc(double peso, double altura, int idade, String sexo) {
+		
+		String retorno = "";
+		imc = peso / (altura*altura);
+		
+		//se for crianca
+		if (idade < 20) 
+			{
+				retorno = "";
+				
+				if (sexo.equals("m")) 
+				{
+					if (imc <= 5.0) {
+						retorno = "Baixo peso";
+					}
+					else if (imc >= 5.1 && imc <= 10.0) {
+						retorno = "Peso normal";
+					}
+					else if (imc >= 10.1 && imc <= 15.0) {
+						retorno = "Sobrepeso";
+					}
+					else if (imc >= 15.1) {
+						retorno = "Obesidade";
+					}
+				}
+				if (sexo.equals("f")) {
+					
+					if (imc <= 4.5) {
+						retorno = "Baixo peso";
+					}
+					else if (imc >= 4.6 && imc <= 12.0) {
+						retorno = "Peso normal";
+					}
+					else if (imc >= 12.1 && imc <= 22.0) {
+						retorno = "Sobrepeso";
+					}
+					else if (imc >= 22.1) {
+						retorno = "Obesidade";
+					}
+				}
+			}
+		//se for adulto
+		else if (idade > 20 || idade < 65) 
+		{				
+			if (imc <= 15.99) { 
+				retorno = "Baixo peso muito grave";
+			}
+			else if (imc >= 16.00 && imc <= 16.99) { 
+				retorno = "Baixo peso grave";
+			}
+			else if (imc >= 17.00 && imc <= 18.49) { 
+				retorno = "Baixo peso";
+			}
+			else if (imc >= 18.50 && imc <= 24.99) {
+				retorno = "Peso normal";
+			}
+			else if (imc >= 25.00 && imc <= 29.99) {
+				retorno = "Sobrepeso";
+			}
+			else if (imc >= 30.00 && imc <= 34.99) {
+				retorno = "Obesidade grau 1";
+			}
+			else if (imc >= 35.00 && imc <= 39.99) {
+				retorno = "Obesidade grau 2";
+			}
+			else if (imc >= 40.00) {
+				retorno = "Obesidade grau 3";
+			}
+		}
+		//se for idoso
+		else if(idade > 65) 
+		{	
+			//idosa
+			if (sexo.equals("f")) 
+			{
+				if (imc <= 21.9) {
+					retorno = "Baixo peso";
+				}
+				else if (imc >= 22.0 && imc <= 27.0) {
+					retorno = "Peso normal";
+				}
+				else if (imc >= 27.1 && imc <= 32.0) {
+					retorno = "Sobrepeso";
+				}
+				else if (imc >= 32.1 && imc <= 37.0) {
+					retorno = "Obesidade grau 1";
+				}
+				else if (imc >= 37.1 && imc <= 41.9) {
+					retorno = "Obesidade grau 2";
+				}
+				else if (imc >= 42.0) {
+					retorno = "Obesidade grau 3";
+				}
+			} 
+			//idoso
+			else if (sexo.equals("m")) 
+			{
+				if (imc <= 21.9) {
+					retorno = "Baixo peso";
+				}
+				else if (imc >= 22.0 && imc <= 27.0) {
+					retorno = "Peso normal";
+				}
+				else if (imc >= 27.1 && imc <= 30.0) {
+					retorno = "Sobrepeso";
+				}
+				else if (imc >= 30.1 && imc <= 35.0) {
+					retorno = "Obesidade grau 1";
+				}
+				else if (imc >= 35.1 && imc <= 39.9) {
+					retorno = "Obesidade grau 2";
+				}
+				else if (imc >= 40.0) {
+					retorno = "Obesidade grau 3";
+				}
+			}
+		}else{
+			retorno = "Valor Invalido";
+		}	
+		return retorno;
+	}
 }
